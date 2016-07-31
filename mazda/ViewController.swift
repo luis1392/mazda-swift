b6c38a25f089f5a614bab26acbf275f528a2ae6e
Luis Enrique Tierrafria Rodriguez <luis@luiss-imac.lan>
2016-07-31 15:43:32 -0500

    Initial Commit

---
+++ mazda/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mazda
+//
+//  Created by Luis Enrique Tierrafria Rodriguez on 7/31/16.
+//  Copyright © 2016 Luis Enrique Tierrafria Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
